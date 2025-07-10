133be2f3dd8969771d5313d4023dbc277e71d484
harshit-kharayat23 <harshitkharyat15@gmail.com>
2025-07-10 18:10:16 +0530

    build login and signup page for frontend

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,41 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
+const Login = () => {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+  const onLogin = async () => {};
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-white text-2xl my-5" >Login</h1>
+    
 
+      <label htmlFor="email">email</label>
+      <input 
+            className="p-1 bg-white rounded-md text-black"
+            id="email"
+            value={user.email}
+            onChange={(e)=>setUser({...user,email:e.target.value})}
+            type="email" 
+            placeholder="@gmail.com" 
+        />
+      <label htmlFor="password">password</label>
+      <input 
+            className="p-1 bg-white rounded-md text-black"
+            id="email"
+            value={user.password}
+            onChange={(e)=>setUser({...user,password:e.target.value})}
+            type="password" placeholder="password" 
+        />
+        <button className="p-2 m-4 bg-white text-black rounded-md" onClick={onLogin}>Login Here</button>
+        <span>New User ? <Link href="/signup"><span className="text-blue-400 font-bold">Signup Page</span></Link></span>
+    </div>
+  );
+};
 
-const LoginPage=()=>{
-
-        return (
-            <div className="flex justify-center items-center">
-                <h1>Login</h1>
-            </div>
-        )
-
-}
-
-
-export default LoginPage;
+export default Login;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,50 @@
-const SignUp=()=>{
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
-        return (
-            <div className="flex items-center-safe">
-                <h1>Sign Up</h1>
-            </div>
-        )
-
-}
-
+const SignUp = () => {
+  const [user, setUser] = React.useState({
+    userName: "",
+    email: "",
+    password: "",
+  });
+  const onSignUp = async () => {};
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-white text-2xl my-5" >Sign Up</h1>
+    
+      <label htmlFor="userame">username</label>
+      <input
+        className="p-1 bg-white rounded-md text-black"
+        type="text"
+        placeholder="username"
+        id="username"
+        value={user.userName}
+        onChange={(e) => setUser({ ...user, userName: e.target.value })}
+      />
+      <label htmlFor="email">email</label>
+      <input 
+            className="p-1 bg-white rounded-md text-black"
+            id="email"
+            value={user.email}
+            onChange={(e)=>setUser({...user,email:e.target.value})}
+            type="email" 
+            placeholder="@gmail.com" 
+        />
+      <label htmlFor="password">password</label>
+      <input 
+            className="p-1 bg-white rounded-md text-black"
+            id="email"
+            value={user.password}
+            onChange={(e)=>setUser({...user,password:e.target.value})}
+            type="password" placeholder="password" 
+        />
+        <button className="p-2 m-4 bg-white text-black rounded-md" onClick={onSignUp}>Sign Up</button>
+        <span>Already a User ? Visit  <Link href="/login"><span className="text-blue-400 font-bold">Login Page</span></Link></span>
+    </div>
+  );
+};
 
 export default SignUp;
